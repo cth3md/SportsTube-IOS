fcb69ed894f6cb5eaef11a83a00635c40ec89527
C. Taylor Hasty <cth3md@mst.edu>
2017-11-03 15:53:36 -0500

    Update AppDelegate.swift

--- AppDelegate.swift
+++ AppDelegate.swift
@@ -6,7 +6,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
+// Creating all the UI view. All constraints are listed
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         
         window = UIWindow(frame: UIScreen.mainScreen().bounds)
