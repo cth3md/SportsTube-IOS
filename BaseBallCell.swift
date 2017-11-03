708c513ac292586699b1a4b90dab11cdc9205187
C. Taylor Hasty <cth3md@mst.edu>
2017-11-03 15:54:23 -0500

    Update BaseBallCell.swift

--- BaseBallCell.swift
+++ BaseBallCell.swift
@@ -2,6 +2,7 @@ import UIKit
 
 class BaseBallCell: FeedCell {
 
+// Retrieves all of the vidoes that are in the BaseBall feed    
     override func fetchVideos() {
         ApiService.sharedInstance.fetchBaseBallFeed { (videos) in
             self.videos = videos
