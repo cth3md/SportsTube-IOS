a83af6e2f77f7b8922584b77315d236d890a2c0a
C. Taylor Hasty <cth3md@mst.edu>
2017-11-03 15:54:48 -0500

    Update NBACell.swift

--- NBACell.swift
+++ NBACell.swift
@@ -2,7 +2,7 @@ import UIKit
 
 class NBACell: FeedCell {
 
-    
+// Retrieves all of the vidoes that are in the NBA feed    
     override func fetchVideos() {
         ApiService.sharedInstance.fetchNBAFeed { (videos) in
             self.videos = videos
